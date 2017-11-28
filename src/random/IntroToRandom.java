4af252f66cad98b6a97ad516cdced2254d93e951
Love4653 <Kaiya.park@gmail.com>
2017-11-27 18:27:45 -0800

    intro to random, dices, Rock Paper Scissors,

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -8,23 +8,23 @@ import java.util.Random;
 //1. Run the program.  What number appears in the console?
 //   Run it again. Is the number the same?
 
-
 public class IntroToRandom {
 	public static void main(String[] args) {
-		int r = 0;
-		
-		//2. Now make r random. 
-		//   Create an object of the Random class
-		//   Hint: new Random().nextInt()
 
-		//3. Limit the random number between 0 and 100
+		Random r = new Random();
+		int num = r.nextInt(310);
+		num -= 222;
+		// 2. Now make r random.
+		// Create an object of the Random class
+		// Hint: new Random().nextInt()
 
-		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
+		// 3. Limit the random number between 0 and 100
 
-		//5. Challenge: Limit the random number between -222 and 88
+		// 4. Limit the random number between 25 and 75. Hint: (highest value - lowest
+		// value) + lowest value
 
+		// 5. Challenge: Limit the random number between -222 and 88
 
-		
-		System.out.println(r); 
+		System.out.println(num);
 	}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -5,23 +5,24 @@ package random;
 
 import java.util.Random;
 
-import javax.swing.JOptionPane;
-
 public class Validation {
 	public static void main(String[] args) {
 
-		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		
+		// 1. OPTIONAL: Watch the first 2 minutes of this movie:
+		// http://vimeo.com/2485018
+
 		int randomNumber = new Random().nextInt(5);
-		
-		// 2. On paper, write all the numbers that get printed when you run this class 10 times
-		
+
+		// 2. On paper, write all the numbers that get printed when you run this class
+		// 10 times
+
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
 
 		// 4. Repeat all the code above 10 times
-		
+
 		// 5. Find someone to test out your program. They will like it :)
 	}
 }
+//
