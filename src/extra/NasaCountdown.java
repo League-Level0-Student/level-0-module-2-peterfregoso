586980766984ff08d093defdeb9acc515a7a3fc2
Garrett-Thomas <league@WTS5.attlocal.net>
2017-12-23 11:27:37 -0800

    extras

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -17,29 +17,27 @@ import javax.swing.JOptionPane;
 public class NasaCountdown {
 	public static void main(String[] args) {
 		// 2. Use a pop-up to ask the user where to start counting from
-String o=JOptionPane.showInputDialog("were shouuld I start the count down?");
+		String p;
+		p = JOptionPane.showInputDialog("were should I start the countdown");
 		// 3. Change the countdown to use the new starting point
-int j;
- j =Integer.parseInt(o);
 
-for (int i = j; i > -1; i--) {
-	System.out.println(i);
-}		
 		// 1. Print a countdown from 10 to 0 on the console
-
-
-		// 4. Use the speak method to hear the countdow
- speak(String o) {
+		int j = Integer.parseInt(p);
+		for (int i = j; i >= 60; i--) {
+			System.out.println(i);
+			speak(i + "");
+		}
+		speak("Blastoff!");
+		// 4. Use the speak method to hear the countdown
 		// 5. when the counting is done, speak "blastoff!"
+	}
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
-
-static void speak(String words) {
-   	 try {
-   		 Runtime.getRuntime().exec("say " + words).waitFor();
-   	 } catch (Exception e) {
-   		 e.printStackTrace();
-   	 }
-  }
-}
-
-
--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -4,6 +4,7 @@
 package extra;
 
 import java.net.URI;
+
 import javax.swing.JOptionPane;
 
 public class SoundtrackToLife {
@@ -11,27 +12,34 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
-		
+		int userMood = JOptionPane.showOptionDialog(null, "what is your mood" + "", "Mood", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Happy", "Sad", "Angry" }, null);
+
 		// 2. Their answer is stored in the userMood variable. Print it out.
+		System.out.println(userMood);
+		// 3. If they are in a stressed mood, use the playVideo method to play a calming
+		// song from YouTube.
+		// angry 2
+		// sad is 1
+		// happy is -1
+		if (userMood == (2)) {
+			playVideo("https://www.youtube.com/watch?v=hTWKbfoikeg");
+		} else if (userMood == (1)) {
+			playVideo("https://www.youtube.com/watch?v=-EQ6eHeBrhM");
+		} else if (userMood == 0) {
+			playVideo("https://www.youtube.com/watch?v=2VXacYLcjGA);");
 
-		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
-
-		
-		// 4. Play different songs for other moods.
-
-/**
-* If you can’t think of any, here are some you can use...
-*	http://bit.ly/video-for-happy
-*	http://bit.ly/video-for-sad
-*	http://bit.ly/video-for-angry
-**/
-		
-		// If you are seeing ads at the beginning of your videos, install Adblock.
-
+		}
 	}
+	// 4. Play different songs for other moods.
+
+	/**
+	 * If you can’t think of any, here are some you can use...
+	 * http://bit.ly/video-for-happy http://bit.ly/video-for-sad
+	 * http://bit.ly/video-for-angry
+	 **/
+
+	// If you are seeing ads at the beginning of your videos, install Adblock.
 
 	static void playVideo(String youTubeLink) {
 		try {
@@ -42,6 +50,3 @@ public class SoundtrackToLife {
 		}
 	}
 }
-
-
-
